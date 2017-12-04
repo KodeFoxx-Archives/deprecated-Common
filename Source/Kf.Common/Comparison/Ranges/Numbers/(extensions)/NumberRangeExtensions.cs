08ad32abe5d26d56e4bc7badb938216ca5af71e9
Yves Schelpe <yves.schelpe@kdg.be>
2017-12-04 10:37:32 +0100

    IRange<int>, IRange<long>, IRange<short> extensions for IsInRange. #3

---
+++ Source/Kf.Common/Comparison/Ranges/Numbers/(extensions)/NumberRangeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Kf.Common.Defensive.Possibly;
+
+namespace Kf.Common.Comparison.Ranges.Numbers
+{
+    public class NumberRangeExtensions
+    {
+        public static bool IsInRange(
+            IRange<short> range, short value, ICompareRange<short> rangeComparer = null
+        ) => rangeComparer
+                .ToPossible().GetValue(new CompareInt16Range())
+                .IsInRange(value, range);
+
+        public static bool IsInRange(
+            IRange<int> range, int value, ICompareRange<int> rangeComparer = null
+        ) => rangeComparer
+            .ToPossible().GetValue(new CompareInt32Range())
+            .IsInRange(value, range);
+
+        public static bool IsInRange(
+            IRange<long> range, long value, ICompareRange<long> rangeComparer = null
+        ) => rangeComparer
+            .ToPossible().GetValue(new CompareInt64Range())
+            .IsInRange(value, range);
+    }
+}
