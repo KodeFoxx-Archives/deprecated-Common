cc492546fa26acc7f82ff3b8c8b04dd90c709bfc
Yves Schelpe <yves.schelpe@gmail.com>
2017-12-05 18:27:01 +0100

    Unit tests for `TestRandom` #8

---
+++ Source/Kf.Common.Tests/Randomization/TestRandomTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Kf.Common.Comparison.Ranges.Numbers;
+using Kf.Common.Randomization;
+using Kf.Common.Tests.Infrastructure;
+using Xunit;
+
+namespace Kf.Common.Tests.Randomization
+{
+    public class TestRandomTests
+    {
+        [Fact]
+        public void Implements_IRandom_interface()
+            => AssertExtensions.ImplementsInterface<IRandom<string>>(
+                TestRandom<string>.Create("testValue")
+            );
+
+        [Fact]
+        public void Next_always_returns_the_same_value() {
+            var expected = "testValue";
+            var sut = TestRandom<string>.Create(expected);
+            Assert.Equal(expected, sut.Next());
+            Assert.Equal(expected, sut.Next(null));
+        }
+
+        [Fact]
+        public void Next_with_range_always_returns_the_same_value()
+        {
+            var expected = 12;
+            var sut = TestRandom<int>.Create(expected);
+            Assert.Equal(expected, sut.Next(20.ToRange(50)));
+        }
+    }
+}
